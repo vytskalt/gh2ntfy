6125d450018262caba24cf422a944f6d36d3d579
vytskalt <vytskalt@protonmail.com>
2024-05-06 20:32:30 +0300

    basically done

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # gh2ntfy
 
-Forward GitHub notifications to ntfy
+A lightweight application that forwards GitHub notifications to [ntfy](https://github.com/binwiederhier/ntfy)
+
+## Quickstart
+
+Here's a full docker-compose.yml for gh2ntfy:
+
+```yaml
+version: '3'
+services:
+  gh2ntfy:
+    image: ghcr.io/vytskalt/gh2ntfy:0.1.0
+    restart: unless-stopped
+    environment:
+      GITHUB_TOKEN: 'TODO' # GitHub personal access token with the notifications and repo scopes. Get one at https://github.com/settings/tokens
+      NTFY_URL: 'https://ntfy.sh' # Your ntfy server
+```
+
+## Building Docker image
+
+Make sure to have the [Nix package manager](https://nixos.org/download/) installed and then run:
+
+```console
+nix build .#image
+cat result | docker load
+```
--- cmd/gh2ntfy/main.go
+++ cmd/gh2ntfy/main.go
@@ -2,18 +2,98 @@ package main
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
-	"io"
+	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"strconv"
 	"strings"
+	"time"
 )
 
+type Notification struct {
+	Subject struct {
+		Title string `json:"title"`
+		URL   string `json:"url"`
+	} `json:"subject"`
+	Repository struct {
+		FullName string `json:"full_name"`
+	} `json:"repository"`
+	Unread bool `json:"unread"`
+}
+
+type NotificationsResponse struct {
+	Date          string
+	Notifications []Notification
+	PollInterval  int
+}
+
 func main() {
-	token := requiredEnvVar("GITHUB_TOKEN")
-	err := fetchNotifications(token)
-	// err := sendToNtfy()
-	fmt.Printf("the: %v\n", err)
+	githubToken := requiredEnvVar("GITHUB_TOKEN")
+	ntfyURL := requiredEnvVar("NTFY_URL")
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		signalCh := make(chan os.Signal, 1)
+		signal.Notify(signalCh, os.Interrupt)
+		<-signalCh
+		log.Println("Shutting down...")
+		cancel()
+	}()
+
+	log.Println("Listening for notifications...")
+
+	afterDate := ""
+loop:
+	for {
+		resp, err := fetchNotifications(ctx, githubToken, afterDate)
+		if err != nil {
+			log.Printf("Failed to fetch notifications: %v\n", err)
+			if ctx.Err() != nil {
+				break
+			}
+
+			log.Println("Will attempt again in 10s")
+			select {
+			case <-ctx.Done():
+				break
+			case <-time.After(10 * time.Second):
+				continue
+			}
+		}
+
+		start := time.Now()
+		forwarded := 0
+		for _, notification := range resp.Notifications {
+			clickURL, err := fetchHTMLURL(githubToken, notification.Subject.URL)
+			if err != nil {
+				log.Printf("Failed to fetch HTML URL of notification source: %v\n", err)
+			}
+
+			err = sendToNTFY(ntfyURL, clickURL, &notification)
+			if err != nil {
+				log.Printf("Failed to forward notification to NTFY: %v\n", err)
+				continue
+			}
+
+			forwarded++
+		}
+
+		if forwarded > 0 {
+			log.Printf("Forwarded %v notifications\n", forwarded)
+		}
+
+		afterDate = resp.Date
+		select {
+		case <-ctx.Done():
+			break loop
+		case <-time.After(time.Duration(resp.PollInterval)*time.Second - time.Since(start)):
+			continue
+		}
+	}
 }
 
 func requiredEnvVar(name string) string {
@@ -25,33 +105,87 @@ func requiredEnvVar(name string) string {
 	return val
 }
 
-func fetchNotifications(token string) error {
-	req, err := http.NewRequest("GET", "https://api.github.com/notifications", bytes.NewReader([]byte{}))
+func fetchHTMLURL(token, apiURL string) (string, error) {
+	req, err := http.NewRequest("GET", apiURL, bytes.NewReader([]byte{}))
 	if err != nil {
-		return err
+		return "", err
 	}
-
-	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
-	req.Header.Set("Authorization", "Bearer "+token)
+	setupGitHubHeaders(req, token)
 
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return err
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == 404 {
+		return "", nil // token does not have the repo scope
+	} else if resp.StatusCode != 200 {
+		return "", fmt.Errorf("received unexpected status code %v when trying to get HTML URL of notification source", resp.StatusCode)
 	}
 
-	body, err := io.ReadAll(resp.Body)
+	var source struct {
+		HTMLURL string `json:"html_url"`
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(&source)
 	if err != nil {
-		return err
+		return "", err
 	}
 
-	fmt.Println(string(body))
-	return nil
+	return source.HTMLURL, nil
 }
 
-func sendToNtfy(url string) error {
-	req, _ := http.NewRequest("POST", url, strings.NewReader("contents"))
-	req.Header.Set("Title", "title")
-	req.Header.Set("Tags", "warning,skull")
+func fetchNotifications(ctx context.Context, token, afterDate string) (*NotificationsResponse, error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", "https://api.github.com/notifications", bytes.NewReader([]byte{}))
+	if err != nil {
+		return nil, err
+	}
+	setupGitHubHeaders(req, token)
+	req.Header.Set("If-Modified-Since", afterDate)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	date := resp.Header.Get("Date")
+	pollInterval, err := strconv.Atoi(resp.Header.Get("X-Poll-Interval"))
+	if err != nil {
+		return nil, err
+	}
+
+	var notifications []Notification
+	if resp.StatusCode == 200 {
+		err = json.NewDecoder(resp.Body).Decode(&notifications)
+		if err != nil {
+			return nil, err
+		}
+	} else if resp.StatusCode != 304 {
+		return nil, fmt.Errorf("received unexpected status code %v when trying to poll for notifications", resp.StatusCode)
+	}
+
+	return &NotificationsResponse{
+		Notifications: notifications,
+		Date:          date,
+		PollInterval:  pollInterval,
+	}, nil
+}
+
+func sendToNTFY(ntfyURL, clickURL string, notification *Notification) error {
+	content := notification.Repository.FullName + ": " + notification.Subject.Title
+	req, _ := http.NewRequest("POST", ntfyURL, strings.NewReader(content))
+	req.Header.Set("Title", "GitHub Notification")
+	if clickURL != "" {
+		req.Header.Set("Click", clickURL)
+	}
 	_, err := http.DefaultClient.Do(req)
 	return err
+}
+
+func setupGitHubHeaders(req *http.Request, token string) {
+	req.Header.Set("User-Agent", "github.com/vytskalt/gh2ntfy")
+	req.Header.Set("X-GitHub-Api-Version", "2022-11-28")
+	req.Header.Set("Authorization", "Bearer "+token)
 }
